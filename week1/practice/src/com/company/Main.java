1c4a8ab4a6900af1ac6484e2ea8317a0199c86df
Dan Sajjad <dan.sajjad@gmail.com>
2019-01-17 22:09:15 -0800

    Week 1 - Implement method sumRange

--- week1/practice/src/com/company/Main.java
+++ week1/practice/src/com/company/Main.java
@@ -3,13 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         test(isEligibleForLoan(3000, 1) == false, "passed", "failed", "return false");
         test(isEligibleForLoan(50000, 1) == false, "passed", "failed", "return false");
         test(isEligibleForLoan(51000, 2) == false, "passed", "failed", "return false");
         test(isEligibleForLoan(150000, 2) == false, "passed", "failed", "return false");
         test(isEligibleForLoan(151000, 0) == true, "passed", "failed", "return true");
         test(isEligibleForLoan(51000, 2.5) == true, "passed", "failed", "return true");
+        test(sumRange(1, 10) == 55, "passed", "failed", "return 55");
+
+
+
         Computer myImac = new Computer();
         println(myImac);
 
@@ -20,11 +23,27 @@ public class Main {
 
         test(myImac.equals(myLenovo) == false, "passed", "failed", "should return false");
         test(myWorkLenovo.equals(myLenovo) == true, "passed", "failed", "should return true");
+
+    }
+
+
+
+
     public static boolean isEligibleForLoan(double income, double numYearsAtCurrentWorkPlace) {
         if (income > 150000) return true;
         else if (income < 50000) return false;
         else return numYearsAtCurrentWorkPlace > 2;
     }
+
+
+    public static int sumRange(int start, int end) {
+        int sum = 0;
+        for(int i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
     public static void println(Object line) { System.out.println(line); }
     public static void print(Object line) { System.out.print(line); }
 
