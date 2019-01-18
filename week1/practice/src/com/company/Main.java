c24a26c7a9cba517dacc19bfdefe64b3994a5fab
Dan Sajjad <dan.sajjad@gmail.com>
2019-01-17 22:08:42 -0800

    Week 1 - Implement function isEligibleForLoan

--- week1/practice/src/com/company/Main.java
+++ week1/practice/src/com/company/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        test(isEligibleForLoan(3000, 1) == false, "passed", "failed", "return false");
+        test(isEligibleForLoan(50000, 1) == false, "passed", "failed", "return false");
+        test(isEligibleForLoan(51000, 2) == false, "passed", "failed", "return false");
+        test(isEligibleForLoan(150000, 2) == false, "passed", "failed", "return false");
+        test(isEligibleForLoan(151000, 0) == true, "passed", "failed", "return true");
+        test(isEligibleForLoan(51000, 2.5) == true, "passed", "failed", "return true");
         Computer myImac = new Computer();
         println(myImac);
 
@@ -14,6 +20,11 @@ public class Main {
 
         test(myImac.equals(myLenovo) == false, "passed", "failed", "should return false");
         test(myWorkLenovo.equals(myLenovo) == true, "passed", "failed", "should return true");
+    public static boolean isEligibleForLoan(double income, double numYearsAtCurrentWorkPlace) {
+        if (income > 150000) return true;
+        else if (income < 50000) return false;
+        else return numYearsAtCurrentWorkPlace > 2;
+    }
     public static void println(Object line) { System.out.println(line); }
     public static void print(Object line) { System.out.print(line); }
 
