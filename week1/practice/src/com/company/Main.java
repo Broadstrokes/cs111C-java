6f563856ea1d1914bc20d6ceea1091b4ef04c5d2
Dan Sajjad <dan.sajjad@gmail.com>
2019-01-17 22:07:49 -0800

    Week 1 - Add helper methods to Driver program

--- week1/practice/src/com/company/Main.java
+++ week1/practice/src/com/company/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    public static void println(Object line) { System.out.println(line); }
+    public static void print(Object line) { System.out.print(line); }
+
+    /**
+     * Function to test an expectation
+     * @param expression     Expectation/Expression to test, which returns a boolean
+     * @param successMessage String to output to the user if the expectation is met
+     * @param errorMessage   String to output to the user if the expectation is not met
+     * @param should         String stating what should be tested
+     */
+    public static void test (boolean expression, String successMessage, String errorMessage, String should) {
+        System.out.print(should + ": ");
+
+        if (expression)
+            System.out.println(successMessage);
+        else
+            System.out.println(errorMessage);
     }
 }
