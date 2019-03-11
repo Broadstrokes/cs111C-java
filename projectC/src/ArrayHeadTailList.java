adfd3b2201a723b6390f0c390f6bf778947bb4ed
Dan Sajjad <dan.sajjad@gmail.com>
2019-03-10 20:05:01 -0700

    Project C - initial commit - create skeleton

---
+++ projectC/src/ArrayHeadTailList.java
@@ -0,0 +1,54 @@
+public class ArrayHeadTailList<T> implements HeadTailListInterface<T> {
+    private T[] listArray;
+    private int numberOfElements;
+
+    @Override
+    public void addFront(T newEntry) {
+
+    }
+
+    @Override
+    public void addBack(T newEntry) {
+
+    }
+
+    @Override
+    public T removeFront() {
+        return null;
+    }
+
+    @Override
+    public T removeBack() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T getEntry(int givenPosition) {
+        return null;
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public int contains(T anEntry) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
