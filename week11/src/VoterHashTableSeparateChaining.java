45d64499a1e4a0fd1f44e30384f1fa04721e38db
Dan Sajjad <dan.sajjad@gmail.com>
2019-04-08 22:34:55 -0700

    Week 11: Implement getVoter & addVoter methods in the Voter hash table

--- week11/src/VoterHashTableSeparateChaining.java
+++ week11/src/VoterHashTableSeparateChaining.java
@@ -15,13 +15,45 @@ public class VoterHashTableSeparateChaining {
 	}
 
 	public boolean addVoter(Voter voterToAdd) {
-		// YOUR CODE HERE
-		return false;
+		int hashIndex = getHashLocation(voterToAdd.getId());
+		Node<Voter> newNode = new Node<>(voterToAdd);
+
+		if (hashTable[hashIndex] == null) {
+			hashTable[hashIndex] = newNode;
+		} else {
+			Node<Voter> currentNode = hashTable[hashIndex];
+			while (currentNode != null) {
+				if (currentNode.getData().getId() == voterToAdd.getId()) {
+					System.out.println(">>>>>>>>>>>>>>>>>> Voter already present");
+					return false;
+				} else if (currentNode.getNextNode() == null) {
+					currentNode.next = newNode;
+					return true;
+				} else {
+					currentNode = currentNode.getNextNode();
+				}
+			}
+		}
+
+		return true;
 	}
 
 	public Voter getVoter(int voterID) {
-		// YOUR CODE HERE
-		return null;
+		int hashIndex = getHashLocation(voterID);
+		if (hashTable[hashIndex] == null) {
+			return null;
+		} else {
+			Node<Voter> currentNode = hashTable[hashIndex];
+			while (currentNode != null) {
+//				System.out.println("Looking for: " + voterID + " | Current Id: " + currentNode.getData().getId());
+				if (currentNode.getData().getId() == voterID) {
+					return currentNode.getData();
+				}
+				currentNode = currentNode.getNextNode();
+			}
+			System.out.println(">>>>>>>>>>>>>> Voter not found");
+			return null;
+		}
 	}
 
 	public void printTable() {
