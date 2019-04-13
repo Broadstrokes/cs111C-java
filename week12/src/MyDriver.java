e43633753357d23d9899c2df8d5182297178190b
Dan Sajjad <dan.sajjad@gmail.com>
2019-04-13 16:42:14 -0700

    Week 12: Create Driver & Util class to test code

---
+++ week12/src/MyDriver.java
@@ -0,0 +1,10 @@
+@SuppressWarnings("Duplicates")
+public class MyDriver {
+
+    public static void main(String[] args) {
+        DriverUtil.printDashes();
+        DriverUtil.println("Starting my driver for Week 12");
+        DriverUtil.printDashes();
+        DriverUtil.println("");
+    }
+}
