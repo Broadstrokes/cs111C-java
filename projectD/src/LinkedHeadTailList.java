36fa32e1550769a67d4a0484a86ba78f91460d4a
Dan Sajjad <dan.sajjad@gmail.com>
2019-04-14 22:29:32 -0700

    Project D: Implement the interface

--- projectD/src/LinkedHeadTailList.java
+++ projectD/src/LinkedHeadTailList.java
@@ -1,57 +1,168 @@
 public class LinkedHeadTailList<T extends Comparable<? super T>> implements HeadTailListInterface<T> {
     public Node firstNode;
     public Node lastNode;
+    private int numberOfEntries;
+
+    public LinkedHeadTailList() {
+        initializeDataFields();
+    }
 
 
     @Override
     public void addFront(T newEntry) {
+        Node newNode = new Node(newEntry);
 
+        if (isEmpty()) {
+            lastNode = newNode;
+        } else { // Add to end of nonempty list
+            newNode.setNextNode(firstNode);
+        } // end if
+
+        firstNode = newNode;
+        numberOfEntries++;
     }
 
     @Override
     public void addBack(T newEntry) {
+        Node newNode = new Node(newEntry);
+
+        if (isEmpty()) {
+            firstNode = newNode;
+            lastNode = newNode;
+        }
+        else { // Add to end of nonempty list
+            lastNode.setNextNode(newNode);
+            lastNode = newNode;
+        } // end if
+
+        numberOfEntries++;
 
     }
 
     @Override
     public T removeFront() {
-        return null;
+        if (!isEmpty()) {
+            T entryToReturn = firstNode.getData();
+            Node newFirst = firstNode.getNextNode();
+            if (numberOfEntries == 1) {
+                lastNode = newFirst;
+            }
+            numberOfEntries--;
+            firstNode = newFirst;
+            return entryToReturn;
+        }
+        else { // Add to end of nonempty list
+            return null;
+        } // end if
     }
 
     @Override
     public T removeBack() {
-        return null;
+        Node currentNode = firstNode;
+        Node previousNode = null;
+        while (currentNode != null && currentNode.getNextNode() != null) {
+            previousNode = currentNode;
+            currentNode = currentNode.getNextNode();
+        }
+
+        if (previousNode != null) previousNode.next = null;
+        if (currentNode != null) {
+            numberOfEntries--;
+            lastNode = previousNode;
+            return currentNode.getData();
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void clear() {
-
+        initializeDataFields();
     }
 
     @Override
     public T getEntry(int givenPosition) {
-        return null;
+        T entryToReturn = null;
+        Node currentNode = firstNode;
+        int currentPosiiton = 0;
+        while (currentNode != null) {
+            if (currentPosiiton == givenPosition) {
+                return currentNode.getData();
+            } else {
+                currentNode = currentNode.getNextNode();
+                currentPosiiton++;
+            }
+        }
+
+        return entryToReturn;
     }
 
     @Override
+    /**
+     * Displays the list
+     * Sample output: [3, 4, 6, 2]	head=3	tail=2"
+     */
     public void display() {
+        String output = "[";
+        Node currentNode = firstNode;
 
+        while (currentNode != null) {
+            output+= currentNode.getData();
+            currentNode = currentNode.getNextNode();
+            if(currentNode != null) {
+                output += ", ";
+            }
+        }
+
+        output += "]";
+
+        if (firstNode != null && lastNode != null) {
+            output += "\t";
+            output += "head="+ firstNode.getData();
+            output += "\t";
+            output += "tail=" + lastNode.getData();
+        }
+
+        System.out.println(output);
     }
 
     @Override
     public int contains(T anEntry) {
-        return 0;
+        boolean found = false;
+        int position = -1;
+        Node currentNode = firstNode;
+
+        while (!found && (currentNode != null)) {
+            position++;
+            if (anEntry.equals(currentNode.getData()))
+                found = true;
+            else
+                currentNode = currentNode.getNextNode();
+        } // end while
+
+        return found ? position : -1;
     }
 
     @Override
     public int size() {
-        return 0;
+        return numberOfEntries;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return numberOfEntries == 0;
     }
+
+    /*
+        HELPER METHODS
+     */
+
+    // Initializes the class's data fields to indicate an empty list.
+    private void initializeDataFields() {
+        firstNode = null;
+        lastNode = null;
+        numberOfEntries = 0;
+    } // end initializeDataFields
 
 
     /**
